c46c30c8330878c2a6e5eb61d60e515f262d5147
waseq <abdulwaseqsultani@gmail.com>
2024-05-18 07:08:43 +0430

    create about page

---
+++ app/about/page.js
@@ -0,0 +1,16 @@
+
+const About =()=>{
+    return(
+        <div className="max-w-xl mx-auto p-3 space-y-4 text-justify">
+            <h1 className="text-2xl text-amber-500 font-medium">About</h1>
+            <p className="">
+                Welcome to our movie databse website! we are a team fo passionate movie enthusiasts who have come together to create one-step destination fo all your movie-related needs. Our website is designed to provide you whit a comprehensive database fo movies from around the world, along with the latest news, reviews, and trailers. Our movie database is constantly updated with new releases, ensuring that you have access to the latest and greatest in director, actor, genre, or release data, making it easy to find the perfect movie for any occasion.
+            </p>
+            <p>
+                In addition to our extensive movie database, we also offer a platform for movie lovers to connect and share their thoughts on the latest releases. Our community section include a forum where you can discuss your favorite films with like-minded individuals and read reviews and ratings from other users. we also have a section dedicated to movie news and trailrs, keeping you updated with the latest happening in the world of cinema.
+                </p>
+        </div>
+    )
+}
+
+export default About
