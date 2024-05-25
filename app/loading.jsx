99f360fde65fb9a657f7e94997407fcb4adbe664
waseq <abdulwaseqsultani@gmail.com>
2024-05-25 11:51:04 +0430

    add loading icon

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,8 @@
 const loading=()=>{
     return(
-        <div className="p-12 text-center text-lg font-bold text-amber-500">
-            <h1>loading...</h1>
+        <div className="p-12 text-center mx-auto items-center text-lg font-bold text-amber-500">
+            <img src="/loadicon.svg" alt="...loading" className="mx-auto" />
+
         </div>
     )
 }
--- components/card.tsx
+++ components/card.tsx
@@ -21,7 +21,7 @@ const Card=({movie}:{movie:Movie})=>{
        
         <div className="group cursor-pointer sm:hover:shadow-slate-400  sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
         <Link href={`/movie/${movie.id}`} >
-            <Image src={`https://image.tmdb.org/t/p/original${movie.backdrop_path || movie.poster_path}`} width={300} height={300} alt="movie image" className=" sm:rounded-t-lg rounded-lg w-full cursor-pointer group-hover:opacity-100 transition-opacity duration-300"></Image>
+            <Image src={`https://image.tmdb.org/t/p/original${movie.backdrop_path || movie.poster_path}`} width={300} height={300} alt="movie image" className=" sm:rounded-t-lg rounded-t-lg w-full cursor-pointer group-hover:opacity-100 transition-opacity duration-300"></Image>
             {/* <img src={"image.jpg"} width={300} height={300} alt="movie image" className=" sm:rounded-t-lg  w-full cursor-pointer group-hover:opacity-100 transition-opacity duration-300"/> */}
         </Link >
         <div className="p-2">
